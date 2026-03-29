9519c60cd273aaaad945be03479fbfcd84b63b0f
Javier534243 <lopezchaconjavier@fpllefia.com>
2026-03-29 12:13:28 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-import {destinacionesDestacados, ofertas, hotelesPopulares} from "./data.js"
+import { destinacionesDestacados, ofertas, hotelesPopulares } from "./data.js";
 
 function dqs(sel) {
-    return document.querySelector(sel)
+  return document.querySelector(sel);
 }
 
-const destinacion = dqs("#destinacionsDestacades")
-let lista = '<ul class="flex quit-all-paddins normal-gap quit-point wrap-media-1024px justify-content-center">'
-for(const des of destinacionesDestacados) {
-    lista += `<li class="total-width" id="${des.id}">
+const destinacion = dqs("#destinacionsDestacades");
+let lista = '<ul class="flex quit-all-paddins normal-gap quit-point wrap-media-1024px justify-content-center">';
+for (const des of destinacionesDestacados) {
+  lista += `<li class="total-width" id="${des.id}">
                 <img src="${des.imagenUrl}" alt="${des.nombre}" class="total-max-width object-fit-cover total-width" />
                 <h3 class="little-margins-vertical title-generic-color third-title-size">${des.nombre}</h3>
                 <p class="quit-all-margins text-generic-size">${des.propiedadesCount} properties</p>
-              </li>`
+              </li>`;
 }
-lista += '</ul>'
-destinacion.innerHTML = lista
+lista += "</ul>";
+destinacion.innerHTML = lista;
 
-const secionOfertas = dqs("#secionOfertas")
-lista = '<ul class="flex normal-gap quit-point quit-all-paddins wrap-media-1024px justify-content-center">'
+const secionOfertas = dqs("#secionOfertas");
+lista = '<ul class="flex normal-gap quit-point quit-all-paddins wrap-media-1024px justify-content-center">';
 for (const ofer of ofertas) {
-    lista += `<li class="total-width" id="${ofer.id}">
+  lista += `<li class="total-width" id="${ofer.id}">
               <div class="position-relative over-flow-hidden">
                 <img src="${ofer.imagenUrl}" alt="${ofer.titulo}" class="total-max-width midle-max-height object-fit-cover total-width"/>
                 <div class="position-absolute total-width total-height generic-border-radius top-0 bottom-0 left-0 right-0 flex align-items-end">
@@ -29,10 +29,21 @@ for (const ofer of ofertas) {
                   </div>
                 </div>
               </div>
-            </li>`
+            </li>`;
 }
-lista += '</ul>'
-secionOfertas.innerHTML += lista
-lista = '<ul>'
+lista += "</ul>";
+secionOfertas.innerHTML += lista;
+lista = "<ul>";
 
-const 
+const hoteles = dqs("#section-hoteles-populares");
+lista = '<ul class="flex quit-all-paddins normal-gap quit-point wrap-media-1024px justify-content-center">';
+for (const hotel of hotelesPopulares) {
+  lista += `<li class="total-max-width total-width" id="${hotel.id}">
+                <img src="${hotel.imagenUrl}" alt="${hotel.nombre}" class="total-max-width midle-max-height total-width" />
+                <h3 class="little-margins-vertical title-generic-color third-title-size">${hotel.nombre}</h3>
+                <p class="quit-all-margins text-generic-size">${hotel.propiedadesCount} properties</p>
+              </li>`
+}
+
+lista += "</ul>";
+hoteles.innerHTML = lista;
