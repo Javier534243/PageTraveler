66ac8b7ac16bc3b5476dc1df24a5edc5e2057b4b
Javier534243 <lopezchaconjavier@fpllefia.com>
2026-03-29 10:59:26 +0000

    no me funciona

--- js/data.js
+++ js/data.js
@@ -2,25 +2,49 @@ export const destinacionsDestacades = [
     {
         id: "australia",
         nombre: "Australia",
-        url: "/img/img-index/img-main-01.png",
-        propiedades: 2246,
+        imatgeUrl: "/img/img-index/img-main-01.png",
+        propietatsCount: 2246,
     },
     {
         id: "japan",
         nombre: "Japan",
-        url: "/img/img-index/img-main-02.png",
-        propiedades: 1278,
+        imatgeUrl: "/img/img-index/img-main-02.png",
+        propietatsCount: 1278,
     },
     {
         id: "new-zealand",
-        nom: "New Zealand",
+        nombre: "New Zealand",
         imatgeUrl: "/img/img-index/img-main-03.png",
         propietatsCount: 480,
     },
     {
         id: "greece",
-        nom: "Greece",
+        nombre: "Greece",
         imatgeUrl: "/img/img-index/img-main-04.png",
         propietatsCount: 320,
     }
 ]
+
+export const ofertas = [
+  {
+    id: "sydneys-10-most-fashionable-5-star-hotels",
+    titulo: "Sydney's 10 Most Fashionable 5 Star Hotels",
+    descripcion:
+      "Browse the fastest growing tourism sector in the heart of Australia tourism Capital.",
+    imatgeUrl: "/img/img-index/img-main-05.png",
+  },
+  {
+    id: "vegan-travel",
+    titulo: "Top cities for Vegan Travellers",
+    descripcion:
+      "Top sites where you do not have to worry about being a vegan. Our tourist guide is here...",
+    imatgeUrl: "/img/img-index/img-main-06.png",
+  },
+  {
+    id: "post-covid",
+    titulo: "World's top destinations during and post covid timeline",
+    descripcion:
+      "Pandemic is still here and will be here for a longer time. Here's where your next destination...",
+    imatgeUrl: "/img/img-index/img-main-07.png",
+  },
+];
