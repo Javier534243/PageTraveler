bd30122625cd52d4ce8e6053495c9633daf30722
Javier534243 <lopezchaconjavier@fpllefia.com>
2026-03-29 11:36:36 +0000

    commit

--- js/data.js
+++ js/data.js
@@ -1,27 +1,27 @@
-export const destinacionsDestacades = [
+export const destinacionesDestacados = [
     {
         id: "australia",
         nombre: "Australia",
-        imatgeUrl: "/img/img-index/img-main-01.png",
-        propietatsCount: 2246,
+        imagenUrl: "/img/img-index/img-main-01.png",
+        propiedadesCount: 2246,
     },
     {
         id: "japan",
         nombre: "Japan",
-        imatgeUrl: "/img/img-index/img-main-02.png",
-        propietatsCount: 1278,
+        imagenUrl: "/img/img-index/img-main-02.png",
+        propiedadesCount: 1278,
     },
     {
         id: "new-zealand",
         nombre: "New Zealand",
-        imatgeUrl: "/img/img-index/img-main-03.png",
-        propietatsCount: 480,
+        imagenUrl: "/img/img-index/img-main-03.png",
+        propiedadesCount: 480,
     },
     {
         id: "greece",
         nombre: "Greece",
-        imatgeUrl: "/img/img-index/img-main-04.png",
-        propietatsCount: 320,
+        imagenUrl: "/img/img-index/img-main-04.png",
+        propiedadesCount: 320,
     }
 ]
 
@@ -31,20 +31,47 @@ export const ofertas = [
     titulo: "Sydney's 10 Most Fashionable 5 Star Hotels",
     descripcion:
       "Browse the fastest growing tourism sector in the heart of Australia tourism Capital.",
-    imatgeUrl: "/img/img-index/img-main-05.png",
+    imagenUrl: "/img/img-index/img-main-05.png",
   },
   {
     id: "vegan-travel",
     titulo: "Top cities for Vegan Travellers",
     descripcion:
       "Top sites where you do not have to worry about being a vegan. Our tourist guide is here...",
-    imatgeUrl: "/img/img-index/img-main-06.png",
+    imagenUrl: "/img/img-index/img-main-06.png",
   },
   {
     id: "post-covid",
     titulo: "World's top destinations during and post covid timeline",
     descripcion:
       "Pandemic is still here and will be here for a longer time. Here's where your next destination...",
-    imatgeUrl: "/img/img-index/img-main-07.png",
+    imagenUrl: "/img/img-index/img-main-07.png",
+  },
+];
+
+export const hotelesPopulares = [
+  {
+    id: "lakeside-motel-warefront",
+    nombre: "Lakeside Motel Warefront",
+    imagenUrl: "/img/img-index/img-main-08.png",
+    propiedadesCount: 2246,
+  },
+  {
+    id: "rocce-graham-resort",
+    nombre: "Rocce Graham resort",
+    imagenUrl: "/img/img-index/img-main-09.png",
+    propiedadesCount: 1278,
+  },
+  {
+    id: "fireside-dinners",
+    nombre: "Fireside Dinners",
+    imagenUrl: "/img/img-index/img-main-10.png",
+    propiedadesCount: 480,
+  },
+  {
+    id: "oculous-inn-stay",
+    nombre: "Oculous Inn Stay",
+    imagenUrl: "/img/img-index/img-main-11.png",
+    propiedadesCount: 320,
   },
 ];
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {destinacionsDestacades, ofertas} from "./data.js"
+import {destinacionesDestacados, ofertas, hotelesPopulares} from "./data.js"
 
 function dqs(sel) {
     return document.querySelector(sel)
@@ -6,21 +6,22 @@ function dqs(sel) {
 
 const destinacion = dqs("#destinacionsDestacades")
 let lista = '<ul class="flex quit-all-paddins normal-gap quit-point wrap-media-1024px justify-content-center">'
-for(const des of destinacionsDestacades) {
-    lista += `<li class="total-width">
-                <img src="${des.imatgeUrl}" alt="${des.nombre}" class="total-max-width object-fit-cover total-width" />
+for(const des of destinacionesDestacados) {
+    lista += `<li class="total-width" id="${des.id}">
+                <img src="${des.imagenUrl}" alt="${des.nombre}" class="total-max-width object-fit-cover total-width" />
                 <h3 class="little-margins-vertical title-generic-color third-title-size">${des.nombre}</h3>
-                <p class="quit-all-margins text-generic-size">${des.propietatsCount} properties</p>
+                <p class="quit-all-margins text-generic-size">${des.propiedadesCount} properties</p>
               </li>`
 }
 lista += '</ul>'
 destinacion.innerHTML = lista
-const ofertas = dqs("#ofertas")
+
+const secionOfertas = dqs("#secionOfertas")
 lista = '<ul class="flex normal-gap quit-point quit-all-paddins wrap-media-1024px justify-content-center">'
 for (const ofer of ofertas) {
-    lista += `<li class="total-width">
+    lista += `<li class="total-width" id="${ofer.id}">
               <div class="position-relative over-flow-hidden">
-                <img src="${ofer.imatgeUrl}" alt="${ofer.titulo}" class="total-max-width midle-max-height object-fit-cover total-width"/>
+                <img src="${ofer.imagenUrl}" alt="${ofer.titulo}" class="total-max-width midle-max-height object-fit-cover total-width"/>
                 <div class="position-absolute total-width total-height generic-border-radius top-0 bottom-0 left-0 right-0 flex align-items-end">
                   <div class="position-relative z-index-increase text-color-white put-all-middle-padding">
                     <h3 class="big-bold-text third-title-size">${ofer.titulo}</h3>
@@ -31,5 +32,7 @@ for (const ofer of ofertas) {
             </li>`
 }
 lista += '</ul>'
-ofertas.innerHTML += lista
+secionOfertas.innerHTML += lista
 lista = '<ul>'
+
+const 
