9c12ceda88222c5678c8b7e86b3add973a9f9a57
Javier534243 <lopezchaconjavier@fpllefia.com>
2026-03-30 21:53:06 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ checkblur("#checkInDate-input")
 checkFocus("#checkOutDate-input")
 checkblur("#checkOutDate-input")
 
- destinacion = dqs("#destinacionsDestacades");
+const destinacion = dqs("#destinacionsDestacades");
 let lista = '<ul class="flex quit-all-paddins normal-gap quit-point wrap-media-1024px justify-content-center">';
 for (const des of destinacionesDestacados) {
   lista += `<li class="total-width" id="${des.id}">
